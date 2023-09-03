96c8b9b3485a815c2af1fafc67a1d1baf055d096
Future <futureworldzone@gmail.com>
2023-09-03 13:30:40 +0300

    Completed assignment with the README.md instructions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,24 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    # creating a Rails session with a variable name of page_views
+    session[:page_views] ||= 0
+    
+    # incrementing page_views when the React page is viewed
+    session[:page_views] += 1
+
+    # if else statement to check if page_views is 3 or less
+    if session[:page_views] < 4
+
+    # original code to load articles
     article = Article.find(params[:id])
     render json: article
+    
+    # if page_views is greater than 3 (4 and above) or less than zero
+    else
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+    end
+
   end
 
   private
